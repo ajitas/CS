11bfeab6bf6ef8d1930ca5144f3e8134e13afea0
Ajita Srivastava <ajitas4000@gmail.com>
2018-09-17 11:54:01 -0700

    adding quicksort

---
+++ sorting/quicksort.js
@@ -0,0 +1,16 @@
+function quicksort(arr){
+    if(arr.length===1)
+        return arr;
+    var pivot = arr[arr.length-1];
+    var left =[];
+    var right = [];
+    for(var i=0;i<arr.length-1;i++){
+        if(arr[i]<=pivot)
+            left.push(arr[i]);
+        else if(arr[i]>=pivot)
+            right.push(arr[i])
+    }
+    return quicksort(left)+ " "+pivot+" "+quicksort(right);
+}
+
+console.log(quicksort([45,3,76,12,54]));
